de0a1aefa7d463b641a32d788de9d6ecdb1f290c
pamallika <sasha.fedorenko4736@gmail.com>
2022-03-31 02:26:36 +0300

    first

---
+++ index.php
@@ -0,0 +1,238 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Мастерок</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/temp.css">
+</head>
+
+<body>
+    <header class="header">
+        <div class="header__left-block">
+            <div class="header__logo">
+                <h2>МастерОк</h2>
+            </div>
+
+        </div>
+        <div class="header__right-block">
+            <button data-modal class="btn btn_white">Связаться с нами</button>
+        </div>
+    </header>
+
+    <div class="preview">
+        <div class="bgc_blue"></div>
+        <div class="container">
+            <div class="tabcontainer">
+                <div class="tabcontent">
+                    <img src="img/tabs/sales.jpg" alt="sale__home">
+                    <div class="tabcontent__descr">
+                        «Мардрахам» — удобное и оригинальное помещение для творческих людей и молодежи.
+                        Компактный и
+                        функциональный дом способен разместить небольшую семью из 3-4 человек с комфортом.
+                    </div>
+                </div>
+                <div class="tabcontent">
+                    <img src="img/tabs/singl.jpg" alt="singl__floor__home">
+                    <div class="tabcontent__descr">
+                        «Барнева» 6х11 — компактный проект для тех, кто ищет недорогие дачные дома. Это одноэтажное
+                        строение с одной жилой комнатой. Общая площадь — 57,9 м². Пригодно как для летнего, так и для
+                        круглогодичного проживания.
+                    </div>
+                </div>
+                <div class="tabcontent">
+                    <img src="img/tabs/small.jpg" alt="small__cache__home">
+                    <div class="tabcontent__descr">
+                        «Енисей-2» 6х9 — компактный проект для тех, кто ищет дачный дом недорого. Это двухэтажное
+                        строение с одной жилой комнатой на первом этаже и двумя комнатами на втором. Общая площадь —
+                        81,8 м². Пригодно как для летнего, так и для круглогодичного проживания.
+                    </div>
+                </div>
+                <div class="tabcontent">
+                    <img src="img/tabs/cottedzh.jpg" alt="cottedzh__home">
+                    <div class="tabcontent__descr">
+                        «Елай» 9х10 — реализация вашей мечты о жизни за пределами города, окунувшегося в смог.
+                        Двухэтажный проект каркасного дома содержит современные конструктивные решения. Второй этаж —
+                        удобное и оригинальное помещение для творческих людей и молодежи.
+                    </div>
+                </div>
+                <div class="tabheader">
+                    <h3>Выберите стиль питания</h3>
+                    <div class="tabheader__items">
+                        <div class="tabheader__item tabheader__item_active">Дома со скидкой</div>
+                        <div class="tabheader__item">Одноэтажные</div>
+                        <div class="tabheader__item">Недорогие дома</div>
+                        <div class="tabheader__item">Коттеджи</div>
+                    </div>
+                </div>
+            </div>
+            <div class="preview__life">Живи полной жизнью!</div>
+        </div>
+    </div>
+
+    <div class="divider"></div>
+
+    <div class="offer">
+        <div class="bgc_y"></div>
+        <div class="container">
+            <div class="offer__text">
+                <h2 class="title">Что мы можем вам предложить?</h2>
+                <div class="offer__descr">
+                    Компания МастерОк занимается строительством домов из бруса и по каркасной технологии в Москве и
+                    Московской области. Мы готовы предложить нашим клиентам полный комплекс услуг — от разработки
+                    проекта до декоративной отделки. Принимаем заказы на строительство каркасных домов под ключ теплых
+                    зимних коттеджей, дач, садовых, бань, гаражей.
+                </div>
+            </div>
+            <div class="offer__action">
+                <button data-modal class="btn btn_dark">Связаться с нами</button>
+            </div>
+        </div>
+        <div class="container">
+            <div class="offer__advantages">
+                <h2>Огромный выбор</h2>
+                <div class="offer__advantages-text">
+                    На нашем сайте представлены готовые проекты брусовых
+                    и каркасных домов и фото завершенных объектов.
+
+                </div>
+                <h2>Гибко</h2>
+                <div class="offer__advantages-text">
+                    Любые проекты наших домов мы бесплатно изменим в
+                    соответствии с пожеланиями заказчика — внесем правки в планировку и комплектацию, изменим площадь
+                    здания, добавим этаж или жилую мансарду, сделаем «зеркальное отражение» проекта.
+                </div>
+            </div>
+            <div class="offer__slider">
+                <div class="offer__slider-counter">
+                    <div class="offer__slider-prev">
+                        <img src="icons/left.svg" alt="prev">
+                    </div>
+                    <span id="current">03</span>
+                    /
+                    <span id="total">04</span>
+                    <div class="offer__slider-next">
+                        <img src="icons/right.svg" alt="next">
+                    </div>
+                </div>
+                <div class="offer__slider-wrapper">
+                    <div class="offer__slider-inner">
+                        <div class="offer__slide">
+                            <img src="img/slider/1.jpg" alt="1">
+                        </div>
+                        <div class="offer__slide">
+                            <img src="img/slider/2.jpg" alt="2">
+                        </div>
+                        <div class="offer__slide">
+                            <img src="img/slider/3.jpg" alt="3">
+                        </div>
+                        <div class="offer__slide">
+                            <img src="img/slider/4.jpg" alt="4">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="promotion">
+            <div class="bgc_y"></div>
+            <div class="container">
+                <div class="promotion__text">
+                    <div class="title">Акция для новых клиентов!</div>
+                    <div class="promotion__descr">
+                        Мы ценим каждого клиента и предлагаем вам стать одним из них на очень выгодных условиях.
+                        Каждому, кто закажет готовые проекты брусовых
+                        и каркасных домовы, будет предоставлена скидка в размере
+                        <span>20%!</span>
+                        <br><br>
+                        Акция закончится 20 мая в 00:00
+                    </div>
+                </div>
+                <div class="promotion__timer">
+                    <div class="title">Осталось до конца акции:</div>
+                    <div class="timer">
+                        <div class="timer__block">
+                            <span id="days"></span>
+
+                        </div>
+                        <div class="timer__block">
+                            <span id="hours"></span>
+
+                        </div>
+                        <div class="timer__block">
+                            <span id="minutes"></span>
+
+                        </div>
+                        <div class="timer__block">
+                            <span id="seconds"></span>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <footer class="footer">
+            <div class="container">
+                <div class="social">
+                    <div class="subtitle">Мы в социальных сетях:</div>
+                    <a href="#" class="link">instagram</a>
+                    <a href="#" class="link">facebook</a>
+                </div>
+                <div class="call">
+                    <div class="subtitle">Или позвоните нам</div>
+                    <a href="#" class="link">+380678341034</a>
+                    <a href="#" class="link">+380500941356</a>
+                </div>
+            </div>
+        </footer>
+
+        <div class="modal">
+            <div class="modal__dialog">
+                <div class="modal__content">
+                    <form method="POST">
+                        <div data-close class="modal__close">&times;</div>
+                        <div class="modal__title">Мы свяжемся с вами как можно быстрее!</div>
+                        <input required placeholder="Ваше имя" name="name" type="text" class="modal__input">
+                        <input required placeholder="Ваш номер телефона" name="phone" type="phone" class="modal__input">
+                        <button type="submit" class="btn btn_dark btn_min">Перезвонить мне</button>
+                        <?php
+                            $connection=mysqli_connect('localhost','root','','masterok');
+                            if($connection){
+                                $name=$_POST['name'];
+                                $phone=$_POST['phone'];
+                                $result=mysqli_query($connection, "INSERT INTO `waiting` (`name`, `phoneNumber`) VALUES ('$name', '$phone');");
+                            }
+                            else{
+                                echo "Ошибка подключения к серверу";
+                                exit();
+                            }
+                        ?>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <div class="works">
+            <?php
+             $request=mysqli_query($connection, "SELECT * FROM `works` ORDER BY `date` DESC");
+            for($i=0;$i<4;$i++){
+                $request_result=mysqli_fetch_assoc($request);
+                echo 
+                '<div class="request">'.
+                '<div class="request__type">'.$request_result['type'].'</div>'.
+                '<div class="request__description">'.$request_result['description'].'</div>'.
+                '<div class="request__cost">'.$request_result['cost'].'</div>'.
+                '</div>';
+                echo'<br>';
+                echo'<hr>';
+            }
+            ?>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+        <script src="js/script.js"></script>
+</body>
+
+</html>
